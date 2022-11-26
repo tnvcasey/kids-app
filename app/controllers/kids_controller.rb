af80c73e10341cfa82ba4ec2edd40d0a1d7f6126
Taylor Vercillo <taylornvcasey@gmail.com>
2022-11-25 18:39:05 -0700

    first

---
+++ app/controllers/kids_controller.rb
@@ -0,0 +1,7 @@
+class KidsController < ApplicationController
+
+    def index
+        kids = Kid.all 
+        render json: kids
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Kid.create(name: "Sam", age:4)
+Kid.create(name: "Jude", age:5)
+Kid.create(name: "Johnny", age:10)
+Kid.create(name: "Cal", age:7)
